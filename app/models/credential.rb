b26894524f7886a11f1e903e923691e7fd20e7d5
Kevin Chandler <im.kevin@me.com>
2015-05-02 19:32:00 -0700

    validate twitter credentials before saving

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,6 +2,8 @@ class Credential < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :user
 
+  before_save :validate_creds
+
 	def twitter_client
 	    client = Twitter::REST::Client.new do |c|
 	      c.consumer_key        = twitter_consumer_key
@@ -12,4 +14,18 @@ class Credential < ActiveRecord::Base
 
 	    return client
 	end
+
+
+  def validate_creds
+    validity = true
+      begin
+        twitter_client.follow('!')
+      rescue Twitter::Error::BadRequest => e
+        validity = false if e.message.index('Bad Authentication data')
+        # validity = true if e.message.index('No user matches for specified terms')
+      ensure
+        return validity
+      end
+    validity
+  end
 end
