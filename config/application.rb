730196ba04f29f65dbd355c2d581d87165eedabe
Kevin Chandler <im.kevin@me.com>
2015-05-10 11:06:21 -0700

    assets

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module Followr
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Pacific Time (US & Canada)'
 
+    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
