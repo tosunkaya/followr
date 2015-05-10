d0b037d82593f583ca0ceedafd9058c67dc1a458
Kevin Chandler <im.kevin@me.com>
2015-05-10 11:32:12 -0700

    check for session before grabbing user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :current_admin
 
   def current_user
-    @current_user || User.find(session[:user_id])
+    User.find(session[:user_id]) if session[:user_id]
   end
 
   def current_admin
-  	@current_admin || User.find(session[:user_id]).twitter_uid == '52296517'
+  	User.find(session[:user_id]).twitter_uid == '52296517' if session[:user_id]
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
+ENV['RAILS_ENV'] ||= 'production'
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
