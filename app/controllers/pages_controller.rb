38ea2fb75f453e7b1a9abba57fa8af99d9e6ab34
Kevin Chandler <im.kevin@me.com>
2015-05-16 08:24:26 -0700

    error handling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,14 +6,14 @@ class PagesController < ApplicationController
 	end
 
 	def dashboard
-		redirect_to root_path and return unless current_user
+		redirect_to root_path and return if current_user.nil?
 
 		if new_user?
 			flash[:notice] = "It looks like you're just getting started - <a href='#{edit_twitter_follow_preference_path(current_user.twitter_follow_preference)}'>Set my follow preferences now</a>".html_safe
 		end
 
 		@followed_users_count = current_user.twitter_follow.count
-		@began_following_users = current_user.twitter_follow.first.created_at.to_date.strftime('%m/%d/%y') if @followed_users_count > 0
-		@followers = current_user.followers.last.count
+		@began_following_users = current_user.twitter_follow.first.created_at.to_date.strftime('%m/%d/%y') rescue nil if @followed_users_count > 0
+		@followers = current_user.followers.last.count if current_user.followers
 	end
 end
