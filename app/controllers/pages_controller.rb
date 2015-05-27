1e6a8fe0b521404d3b355d6b86c1db42f749c78a
Kevin Chandler <im.kevin@me.com>
2015-05-27 07:41:47 -0700

    admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,11 @@ class PagesController < ApplicationController
 		@began_following_users = current_user.twitter_follow.first.created_at.to_date.strftime('%m/%d/%y') rescue nil if @followed_users_count > 0
 		@followers = current_user.followers.last.count if current_user.followers.present?
 	end
+
+	def admin
+		return unless current_admin
+
+		@users = User.all
+	end
+
 end
