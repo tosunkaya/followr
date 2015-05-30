9116cb03d8731a8d349b3a969e0c30db56930191
Kevin Chandler <im.kevin@me.com>
2015-05-29 22:24:45 -0700

    rm validation for unfollow_after

--- app/models/twitter_follow_preference.rb
+++ app/models/twitter_follow_preference.rb
@@ -2,7 +2,7 @@ class TwitterFollowPreference < ActiveRecord::Base
 	belongs_to :user
 
 	validates_presence_of :user
-	validates :unfollow_after, inclusion: { in: [1, 2] }
+	# validates :unfollow_after, inclusion: { in: [1, 2] }
 
 	def rate_limited?
 		rate_limit_until > DateTime.now
