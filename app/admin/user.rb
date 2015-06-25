43a84f55e64e4dadf9dec1c73892f61aa9572c68
Kevin Chandler <im.kevin@me.com>
2015-06-25 11:42:47 -0700

    rm per page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,5 @@
 ActiveAdmin.register User do
 
-  config.per_page = 200
-
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
