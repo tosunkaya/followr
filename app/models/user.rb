9821d324a3b6abdc990678b9b03beea90c26c24a
Michele Piccirillo <michele.piccirillo@gmail.com>
2015-06-26 22:06:48 +0200

    twitter_check? checks if at least one of follow or unfollow is checked

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ActiveRecord::Base
   scope :wants_twitter_unfollow, -> { joins('INNER JOIN twitter_follow_preferences ON (users.id = user_id)').where('twitter_follow_preferences.mass_unfollow IS TRUE') }
 
   after_create :init_follow_prefs
-  
+
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.twitter_uid = auth["uid"]  
+      user.twitter_uid = auth["uid"]
       user.twitter_username = auth["info"]['nickname']
       user.name = auth["info"]["name"]
       Credential.create_with_omniauth(user, auth)
@@ -22,8 +22,7 @@ class User < ActiveRecord::Base
   end
 
   def init_follow_prefs
-    fp = TwitterFollowPreference.new(user: self)
-    twitter_follow_preference = fp
+    self.twitter_follow_preference = TwitterFollowPreference.new(user: self)
   end
 
   def rate_limited?
@@ -36,7 +35,7 @@ class User < ActiveRecord::Base
     hashtags = follow_prefs.hashtags.gsub('#','').split(',')
 
     client = self.credential.twitter_client rescue nil
-    return false if client.nil? || hashtags.empty? || !follow_prefs.want_mass_follow?
+    return false if client.nil? || hashtags.empty? || (!follow_prefs.want_mass_follow? && !follow_prefs.mass_unfollow)
     true
   end
 
@@ -44,7 +43,7 @@ class User < ActiveRecord::Base
     return false unless self.credential.is_valid
     return false if twitter_follow_preference.rate_limit_until > DateTime.now
 
-    followed_in_last_hour = self.twitter_follows.where('followed_at > ?', 1.hour.ago) 
+    followed_in_last_hour = self.twitter_follows.where('followed_at > ?', 1.hour.ago)
     followed_in_last_day = self.twitter_follows.where('followed_at > ?', 24.hours.ago)
     return false if followed_in_last_hour.count >= 30 || followed_in_last_day.count >= 720
     true
@@ -53,7 +52,7 @@ class User < ActiveRecord::Base
   def can_twitter_unfollow?
     return false unless self.credential.is_valid
 
-    unfollowed_in_last_hour = self.twitter_follows.where('unfollowed_at > ?', 1.hour.ago) 
+    unfollowed_in_last_hour = self.twitter_follows.where('unfollowed_at > ?', 1.hour.ago)
     unfollowed_in_last_day = self.twitter_follows.where('unfollowed_at > ?', 24.hours.ago)
     return false if unfollowed_in_last_hour.count >= 50 || unfollowed_in_last_day.count >= 900
     true
