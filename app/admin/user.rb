ec23865d8a8c7ea4f1f1892ab7f865e02d2e05bd
Michele Piccirillo <michele.piccirillo@gmail.com>
2015-06-26 21:28:29 +0200

    Remove comments in admin/user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,25 +4,11 @@ ActiveAdmin.register User do
       User.find_by_id(params[:id])
     end
   end
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
   
   filter :twitter_username
   filter :created_at
 
-  index do 
+  index do
     selectable_column
     id_column
 
