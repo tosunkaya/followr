c4d5dfbb92cb97289e7810b9b40a283bda352fb1
Kevin Chandler <im.kevin@me.com>
2015-06-27 08:43:48 -0700

    follower change, link twitter username

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,10 +13,11 @@ ActiveAdmin.register User do
     id_column
 
     column :name
-    column :twitter_username
-    column :created_at
+    column (:twitter_username) { |u| link_to u.twitter_username, "https://twitter.com/#{u.twitter_username}" }
     column :twitter_check?
     column (:hashtags) { |u| u.hashtags.join(',') }
+    column ('Followers Increase') { |u| u.followers.last.count - u.followers.first.count rescue nil }
+    column :created_at
     actions
   end
 
