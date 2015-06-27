24473df030884d70c962407aaedd8d8c6c3ae699
Kevin Chandler <im.kevin@me.com>
2015-06-27 08:48:16 -0700

    fix col name

--- app/models/twitter_follow_preference.rb
+++ app/models/twitter_follow_preference.rb
@@ -5,7 +5,7 @@ class TwitterFollowPreference < ActiveRecord::Base
 	validates :unfollow_after, inclusion: { in: [1, 2] }
 
 	def rate_limited?
-		!rate_limit.until.nil? && rate_limit_until > DateTime.now
+		!rate_limit_until.nil? && rate_limit_until > DateTime.now
 	end
 
 	def want_mass_follow?
