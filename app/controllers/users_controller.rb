6587c21e4e44643e16443fb1011025e676ffbad9
Kevin Chandler <im.kevin@me.com>
2015-07-02 23:05:32 -0700

    update user api to save their email

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def update
+    begin
+      u = User.find current_user.id
+      u.email = params[:user][:email].downcase
+      u.save!
+    rescue => e
+      Airbrake.notify(e)
+    ensure
+      redirect_to dashboard_path
+    end
+  end
+
+
+  def unfollow
+    begin
+      @twitter_follow = current_user.twitter_follows.find(params[:id])
+      @twitter_follow.unfollow!
+      respond_to do |format|
+        format.js { render inline: "$('#follow-#{@twitter_follow.id}').replaceWith('<%=j render partial: 'twitter_follow', locals: { twitter_follow: @twitter_follow} %>')" }
+      end
+
+    rescue => e
+      Airbrake.notify(e)
+      render js: "alert('Oops! It seems that something went wrong :(')"
+    end
+  end
+
+
+end
